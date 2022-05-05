51a38da40a40e141562418c46c0b719a64e52063
CrypticzXI <plehtide@outlook.com>
2022-05-05 02:02:07 +0100

    Home page

--- pages/index.js
+++ pages/index.js
@@ -98,10 +98,27 @@ const Home = ({
               <h2 className="text-Text font-poppins font-[800] text-2xl md:text-4xl lg:text-5xl uppercase tracking-wide leading-[1]">{home_page[0].portfoliotitle}</h2>
                 <div className="border-[1px] mt-8 mb-[5rem] w-64 border-Text"></div>
 
-                <div className="w-full grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 items-center justify-center">
+                <div className="w-full h-full gap-8 items-start flex flex-wrap transition-all justify-center mt-[2rem]">
+
+                  {thumbnail?
+                    <div className="flex flex-col hover:scale-[0.95] transition-all items-center justify-center space-y-2 w-full md:w-[70%] lg:w-[48%] h-auto">
+                      <img className="rounded-lg w-full h-full" src={urlFor(thumbnail.image)} />
+                      <p className="text-Text pt-4 lg:text-xl md:text-base text-xs font-rubik">{thumbnail.description}</p>
+                    </div>
+                  :null}
+
+                  {images?.map(( data ) => (
+                    <div className="flex flex-col hover:scale-[0.95] transition-all items-center space-y-2 w-full md:w-[70%] lg:w-[48%] h-auto">
+                      <img className="rounded-lg w-full h-full" src={urlFor(data.image)} />
+                      <p className="text-Text pt-4 lg:text-xl md:text-base text-xs font-rubik">{data.description}</p>
+                    </div>
+                  ))}
+                </div>
+
+                <div className="w-full h-full gap-8 items-start flex flex-wrap transition-all justify-center mt-[2rem]">
                     {portfolios.map((portfolio) => (
                       portfolio.thumbnail && portfolio.title && portfolio.owninguser.name?
-                        <a href={`/artwork/${portfolio.slug.current}`} className="w-full h-full hover:scale-[0.95] transition-all relative group rounded-xl flex flex-col items-center justify-center overflow-hidden">       
+                        <a href={`/artwork/${portfolio.slug.current}`} className="w-full md:w-[49%] h-auto lg:w-[24%] hover:scale-[0.95] transition-all relative group rounded-xl flex flex-col items-center justify-center overflow-hidden">       
                             <img className="w-full h-full transition-all" src={urlFor(portfolio.thumbnail.image)} />
                             <div className="absolute bottom-0 left-0 w-full items-center justify-center transition-colors h-[50%] from-transparent via-Background/50 to-Background/70 group-hover:bg-gradient-to-b flex flex-col">
                                 <p className="font-rubik group-hover:text-Text font-[600] text-transparent text-lg">{portfolio.title}</p>
