1875c906540e2923f58ef42ce3bc05b8827a11e2
CrypticzXI <plehtide@outlook.com>
2022-05-05 02:06:52 +0100

    profile

--- pages/profile/[slug].js
+++ pages/profile/[slug].js
@@ -111,9 +111,9 @@ const Profile = ({
         <h2 className="text-Text font-poppins font-[800] text-2xl md:text-4xl lg:text-5xl uppercase tracking-wide leading-[1]">Portfolio</h2>
         <div className="border-[1px] my-8 w-64 border-Text"></div>
         
-        <div className="w-full h-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 items-center justify-center mt-10 lg:max-w-[70%]">
+        <div className="w-full h-full gap-8 items-start flex flex-wrap transition-all justify-center mt-10 lg:max-w-[70%]">
             {Portfolios.map((portfolio) => (
-                <a href={`/artwork/${portfolio.slug.current}`} className="w-full h-auto hover:scale-[0.95] transition-all relative group rounded-xl flex flex-col items-center justify-center overflow-hidden">             
+                <a href={`/artwork/${portfolio.slug.current}`} className="w-full md:w-[49%] h-auto lg:w-[24%] hover:scale-[0.95] transition-all relative group rounded-xl flex flex-col items-center justify-center overflow-hidden">             
                     <img className="w-full h-full transition-all" src={urlFor(portfolio.thumbnail.image)} />
                     <div className="absolute bottom-0 left-0 w-full items-center justify-center transition-colors h-[50%] from-transparent via-Background/50 to-Background/70 group-hover:bg-gradient-to-b flex flex-col">
                         <p className="font-rubik group-hover:text-Text font-[600] text-transparent text-base">{portfolio.title}</p>
