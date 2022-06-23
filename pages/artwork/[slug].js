8c44149159cafd00efd91d04e8153954b0f19c79
CrypticzXI <plehtide@outlook.com>
2022-06-23 19:52:53 +0100

    a

--- pages/artwork/[slug].js
+++ pages/artwork/[slug].js
@@ -40,6 +40,18 @@ const Portfolio = ({
         </div>
       :null} 
 
+      {images?.map(( data ) => (
+        <div id={`image-canvas-${data.id}`} className="absolute w-full hidden min-h-screen h-full top-0 left-0 bg-Background items-center justify-center">
+          <img className="w-[80%] h-auto" src={urlFor(data.image)} />
+          <button onClick={() => {document.getElementById(`thumb-canvas-${data.id}`).style.display = "none"}} className="absolute top-5 right-20 space-x-5 h-10 w-auto flex items-center">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10 text-Text" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+              <path strokeLinecap="round" strokeLinejoin="round" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+            </svg>
+            <h2 className="text-Text font-rubik">Close Image</h2>
+          </button>
+        </div>
+      ))}
+
 
 
       <div className="py-20 flex flex-col items-center px-10 md:px-[4rem] lg:px-40">
