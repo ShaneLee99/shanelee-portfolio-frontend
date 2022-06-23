f4e8063ab5c3f2198e62f5b7597e4ac03faf8ef1
CrypticzXI <plehtide@outlook.com>
2022-06-23 23:46:55 +0100

    a

--- pages/artwork/[slug].js
+++ pages/artwork/[slug].js
@@ -28,29 +28,16 @@ const Portfolio = ({
         <p className="text-Text group-hover:text-FirstColour transition-all font-rubik">Go Back</p>
       </button>
 
-      {thumbnail?
-        <div id="thumb-canvas" className="absolute w-full hidden min-h-screen h-full top-0 left-0 bg-Background items-center justify-center">
-          <img className="w-[80%] h-auto" src={urlFor(thumbnail.image)} />
-          <button onClick={() => {document.getElementById("thumb-canvas").style.display = "none"}} className="absolute top-5 right-20 space-x-5 h-10 w-auto flex items-center">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10 text-Text" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-              <path strokeLinecap="round" strokeLinejoin="round" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
-            </svg>
-            <h2 className="text-Text font-rubik">Close Image</h2>
-          </button>
-        </div>
-      :null} 
-
-      {images?.map(( data ) => (
-        <div id={`image-canvas-${data._key}`} className="absolute w-full hidden min-h-screen h-full top-0 left-0 bg-Background items-center justify-center">
-          <img className="w-[80%] h-auto" src={urlFor(data.image)} />
-          <button onClick={() => {document.getElementById(`thumb-canvas-${data._key}`).style.display = "none"}} className="absolute top-5 right-20 space-x-5 h-10 w-auto flex items-center">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10 text-Text" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-              <path strokeLinecap="round" strokeLinejoin="round" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
-            </svg>
-            <h2 className="text-Text font-rubik">Close Image</h2>
-          </button>
-        </div>
-      ))}
+      
+      <div id="canvas" className="absolute w-full hidden min-h-screen h-full top-0 left-0 bg-Background items-center justify-center">
+        <img className="w-[80%] h-auto" src={urlFor(thumbnail.image)} />
+        <button onClick={() => {document.getElementById("canvas").style.display = "none"}} className="absolute top-5 right-20 space-x-5 h-10 w-auto flex items-center">
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10 text-Text" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+            <path strokeLinecap="round" strokeLinejoin="round" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+          </svg>
+          <h2 className="text-Text font-rubik">Close Image</h2>
+        </button>
+      </div>
 
 
 
@@ -99,14 +86,14 @@ const Portfolio = ({
           <div className="w-full h-full gap-8 items-start flex flex-wrap transition-all justify-center mt-[2rem]">
 
             {thumbnail?
-                <div onClick={() => {document.getElementById("thumb-canvas").style.display = "flex"}} className="flex flex-col hover:scale-[0.95] transition-all items-center justify-center space-y-2 w-full md:w-[70%] lg:w-[48%] h-auto">
+                <div onClick={() => {document.getElementById("canvas").style.display = "flex"; document.getElementById("canvas").src = `${thumbnail.image}`;}} className="flex flex-col hover:scale-[0.95] transition-all items-center justify-center space-y-2 w-full md:w-[70%] lg:w-[48%] h-auto">
                   <img className="rounded-lg w-full h-full" src={urlFor(thumbnail.image)} />
                   <p className="text-Text pt-4 lg:text-xl md:text-base text-xs font-rubik">{thumbnail.description}</p>
                 </div>
             :null}
 
             {images?.map(( data ) => (
-              <div onClick={() => {document.getElementById(`thumb-canvas-${data._key}`).style.display = "flex"}} className="flex flex-col hover:scale-[0.95] transition-all items-center space-y-2 w-full md:w-[70%] lg:w-[48%] h-auto">
+              <div onClick={() => {document.getElementById("canvas").style.display = "flex"; document.getElementById("canvas").src = `${data.image}`;}} className="flex flex-col hover:scale-[0.95] transition-all items-center space-y-2 w-full md:w-[70%] lg:w-[48%] h-auto">
                 <img className="rounded-lg w-full h-full" src={urlFor(data.image)} />
                 <p className="text-Text pt-4 lg:text-xl md:text-base text-xs font-rubik">{data.description}</p>
               </div>
