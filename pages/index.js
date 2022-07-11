0a88ed1e792a235bea8395fa0ea412164b86130d
CrypticzXI <CrypticzXI@gmail.com>
2022-07-11 04:15:27 +0100

    Should work plz

--- pages/index.js
+++ pages/index.js
@@ -41,7 +41,7 @@ const Home = ({
                     <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16" />
                 </svg>
               </button>
-              <div id="mobile_menu" className="bg-SecondryBackground w-full h-screen hidden top-0 left-0 fixed">
+              <div id="mobile_menu" className="bg-SecondryBackground w-full h-screen top-0 left-0 fixed">
                 <div className="flex w-full h-full relative">
                     <button onClick={() => {document.getElementById("mobile_menu").style.display = "none";}} className="absolute top-5 right-5 ">
                       <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-Text transition-all hover:text-FirstColour" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
