699cc994c544b174071b905d43ede043447ac09f
CrypticzXI <CrypticzXI@gmail.com>
2022-07-11 03:49:07 +0100

    yes

--- pages/profile/[slug].js
+++ pages/profile/[slug].js
@@ -113,7 +113,7 @@ const Profile = ({
         
         <div className="w-full h-full gap-8 items-start flex flex-wrap transition-all justify-center mt-10 lg:max-w-[70%]">
             {Portfolios.map((portfolio) => (
-                <a href={`/artwork/${portfolio.slug.current}`} className="w-full md:w-[49%] h-auto lg:w-[24%] hover:scale-[0.95] transition-all relative group rounded-xl flex flex-col items-center justify-center overflow-hidden">             
+                <a href={`/artwork/${portfolio.slug.current}`} className="w-auto md:w-[49%] lg:w-[24%] hover:scale-[0.95] h-full transition-all relative group rounded-xl flex flex-col items-center justify-center overflow-hidden">             
                     <img className="w-full h-full transition-all" src={urlFor(portfolio.thumbnail.image)} />
                     <div className="absolute bottom-0 left-0 w-full items-center justify-center transition-colors h-[50%] from-transparent via-Background/50 to-Background/70 group-hover:bg-gradient-to-b flex flex-col">
                         <p className="font-rubik group-hover:text-Text font-[600] text-transparent text-base">{portfolio.title}</p>
