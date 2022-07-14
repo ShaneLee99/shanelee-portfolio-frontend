ae9868c34e4010fb6b9cbd10f315b91fc267264d
CrypticzXI <CrypticzXI@gmail.com>
2022-07-14 22:54:26 +0100

    Added blogs to home page

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,16 @@
 import { sanityClient, urlFor } from "../sanity"
 import Dynamic from 'next/dynamic';
 import JarallaxImage from '../components/JarallaxImage';
+import moment from 'moment'
 
 const Jarallax = Dynamic(() => import('../components/Jarallax'), { ssr: false });
 
 const home = ({
   portfolios, 
   home_page, 
-  Navbar
+  Navbar,
+  Blog,
+  DateTime
 }) => {
   return (
         <div className="main">
@@ -108,7 +111,39 @@ const home = ({
 
             </div>
 
-          :<div></div>}
+          :null}
+
+        {blogs_exist?
+          
+          <div id="blogs" className="bg-Background flex justify-center">
+
+            <div className="py-20 flex flex-col items-center px-10 md:px-[4rem] lg:px-40 xl:px-60">
+
+            <h2 className="text-Text font-poppins font-[800] text-2xl md:text-4xl lg:text-5xl uppercase tracking-wide leading-[1]">{home_page[0].portfoliotitle}</h2>
+              <div className="border-[1px] mt-8 mb-[5rem] w-64 border-Text"></div>
+
+              <div className="w-full h-full gap-8 items-start flex flex-wrap transition-all justify-center mt-[2rem]">
+                {Blog?.map((skill) => (
+                  Blog?
+                    <a href={`/blog/${skill.slug.current}`} className="w-full md:max-w-4xl max-w-md h-44 bg-SecondryBackground rounded-xl flex items-center md:p-8 p-4 hover:scale-[0.98] transition-all">
+                      <img src={urlFor(skill.image)} className="md:w-[100px] md:h-[100px] h-[75px] w-[75px] rounded-full"></img>
+                      <div className="text-Text ml-5">
+                        <h2 className="font-[500] text-xl font-poppins">{skill.title}</h2>
+                        <div className="font-[300] md:text-base text-xs mt-2 text-Text font-rubik flex md:flex-row flex-col md:space-y-0 space-y-2">
+                          <p className="mr-2">Posted: {DateTime}</p>
+                          {skill.portfolio.title?<p className="md:flex hidden mr-2">/</p>:null}
+                          {skill.portfolio.title?<p className="">Portfolio Refrence: {skill.portfolio.title}</p>:null}
+                        </div>
+                      </div>
+                    </a>
+              :null))}
+              </div>
+
+            </div>
+
+          </div>
+
+        :null}
         </div>
 )}
 
@@ -135,13 +170,35 @@ export const getServerSideProps = async () => {
   }`
   const navbar = await sanityClient.fetch(nav_query)
 
+  const blog_query = `*[_type == "blog"]{
+    title,
+    post,
+    image,
+    slug,
+    date,
+    portfolio->{
+      title
+    },
+    owninguser->{
+      name,
+      image,
+      desc
+    }
+  }`
 
-  if (!portfolios.length) {
+  const blog = await sanityClient.fetch(blog_query)
+
+  const datetime = moment(blogs.date).format(("dddd, MMMM Do YYYY, h:mm:ss a")); 
+
+
+  if (!portfolios.length || !blog.length) {
     return {
       props: {
         portfolios: [],
         home_page,
         Navbar: navbar,
+        Blog: blog,
+        DateTime: datetime
       },
     }
   } else {
@@ -150,6 +207,8 @@ export const getServerSideProps = async () => {
         portfolios,
         home_page,
         Navbar: navbar,
+        Blog: blog,
+        DateTime: datetime
       },
     }
   }
