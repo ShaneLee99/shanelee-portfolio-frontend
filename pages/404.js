770822cf08287732b7e030f44602a1e3183df5bf
CrypticzXI <CrypticzXI@gmail.com>
2022-08-10 07:32:06 +0100

    Footer on 404 hehe

--- pages/404.js
+++ pages/404.js
@@ -10,13 +10,13 @@ const FourOhFour = ({
     DomainName
 }) => {
   return (
-        <div className="main container">
+        <div className="main bg-Background">
 
           <Head>
             <title>404 - Page Not Found</title>
           </Head>
 
-            <div className="bg-Background w-screen h-screen flex flex-col items-center justify-center">
+            <div className="bg-Background h-screen flex flex-col items-center justify-center">
                 <h2 className="text-Text font-poppins font-[800] text-2xl md:text-4xl lg:text-5xl uppercase tracking-wide leading-[1]">404</h2>
                 <div className="border-[1px] mt-8 mb-8 w-64 border-Text"></div>
                 <h2 className="text-Text font-poppins font-[800] text-lg md:text-2xl uppercase tracking-wide leading-[1]">Page not Found.</h2>
